e89bb081292afa5e2902155b176fabcac96ad6e9
allwaysoft <49237778+allwaysoft@users.noreply.github.com>
2022-12-03 12:55:00 +0800

    Add files via upload

--- src/main/java/com/example/LoginLogController.java
+++ src/main/java/com/example/LoginLogController.java
@@ -1,10 +1,17 @@
 package com.example;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,12 +20,38 @@ public class LoginLogController {
     @Autowired
     private LoginLogRepository loginLogRepository;
 
-    @RequestMapping("/loginlog")
-    public String indexPermissions(Model model) {
-        List<LoginLog> listLoginLogs = loginLogRepository.findAllByOrderByIdDesc();
-        model.addAttribute("listLoginLogs", listLoginLogs);
+    @GetMapping("/loginlog")
+    public String getLoginLog(@Param("keyword") String keyword, @PageableDefault(size = 10, sort = "id") Pageable pageable,
+            Model model) {
+
+        if (keyword == null || keyword.trim().isEmpty()) {
+            Page<LoginLog> page = loginLogRepository.findAll(pageable);
+            List<Sort.Order> sortOrders = page.getSort().stream().collect(Collectors.toList());
+            if (sortOrders.size() > 0) {
+                Sort.Order order = sortOrders.get(0);
+                model.addAttribute("sortProperty", order.getProperty());
+                model.addAttribute("sortDesc", order.getDirection() == Sort.Direction.DESC);
+                model.addAttribute("sortDirection", order.getDirection().isAscending() ? "asc" : "desc");
+                model.addAttribute("reverseSortDirection", order.getDirection().isAscending() ? "desc" : "asc");
+            }
+            model.addAttribute("keyword", "");
+            model.addAttribute("page", page);
+        } else {
+            Page<LoginLog> page = loginLogRepository.search(keyword, pageable);
+            List<Sort.Order> sortOrders = page.getSort().stream().collect(Collectors.toList());
+            if (sortOrders.size() > 0) {
+                Sort.Order order = sortOrders.get(0);
+                model.addAttribute("sortProperty", order.getProperty());
+                model.addAttribute("sortDesc", order.getDirection() == Sort.Direction.DESC);
+                model.addAttribute("sortDirection", order.getDirection().isAscending() ? "asc" : "desc");
+                model.addAttribute("reverseSortDirection", order.getDirection().isAscending() ? "desc" : "asc");
+            }
+            model.addAttribute("keyword", keyword);
+            model.addAttribute("page", page);
+        }
 
         return "loginlog/list_loginlog";
+
     }
 
 }
--- src/main/java/com/example/LoginLogRepository.java
+++ src/main/java/com/example/LoginLogRepository.java
@@ -1,10 +1,16 @@
 package com.example;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LoginLogRepository extends JpaRepository<LoginLog, Long> {
 
-    List<LoginLog> findAllByOrderByIdDesc();
+    Page<LoginLog> findAllByOrderByIdDesc(Pageable pageable);
+
+    @Query("SELECT l FROM LoginLog l WHERE CONCAT(l.username, ' ', l.description, ' ', l.ip, ' ', l.sessionid) LIKE %?1%")
+    public Page<LoginLog> search(String keyword, Pageable pageable);
 
 }
